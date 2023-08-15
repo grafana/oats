d49c2824acbc96d4088b2caab92bab226637841d
caleb miles <caselim@gmail.com>
2023-08-15 18:31:18 -0400

    go back to using math/rand for selecting a candidate host port
    ....hopefully less likely to create race conditions on the consumer side

--- internal/testhelpers/common/host_port.go
+++ internal/testhelpers/common/host_port.go
@@ -1,26 +1,37 @@
 package common
 
 import (
+	"fmt"
+	"math/rand"
 	"net"
 )
 
 func HostTCPPort() (string, error) {
-	conn, err := net.Listen("tcp", ":0")
-	if err != nil {
-		return "", err
+	var selectedPort int
+
+	for trials := 0; trials < 10000; trials++ {
+		trialPort := rand.Intn(65535)
+		if trialPort <= 1024 {
+			continue
+		}
+
+		conn, connErr := net.Listen("tcp", fmt.Sprintf(":%d", trialPort))
+		if connErr != nil {
+			continue
+		}
+
+		closeErr := conn.Close()
+		if closeErr != nil {
+			continue
+		}
+
+		selectedPort = trialPort
+		break
 	}
 
-	addr := conn.Addr().String()
-
-	err = conn.Close()
-	if err != nil {
-		return "", err
+	if selectedPort == 0 {
+		return "", fmt.Errorf("could not find open unprivileged port after 10,000 attempts")
 	}
 
-	_, port, err := net.SplitHostPort(addr)
-	if err != nil {
-		return "", err
-	}	
-
-	return port, nil
+	return fmt.Sprintf("%d", selectedPort), nil
 }
