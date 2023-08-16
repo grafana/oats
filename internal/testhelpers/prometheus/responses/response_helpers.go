88fe97b8eb633e85e5eedc9e2a79c5c712ec9e2a
Nikola Grcevski <6207777+grcevski@users.noreply.github.com>
2023-08-16 15:25:32 -0400

    Auto instrument metrics example (#9)
    * Add helpers and auto-instrumentation traces test
    
    * Add support for running metrics test with compose

--- internal/testhelpers/prometheus/responses/response_helpers.go
+++ internal/testhelpers/prometheus/responses/response_helpers.go
@@ -3,6 +3,7 @@ package responses
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 )
 
 func ParseQueryOutput(body []byte) ([]Result, error) {
@@ -12,4 +13,31 @@ func ParseQueryOutput(body []byte) ([]Result, error) {
 	}
 
 	return qr.Data.Result, nil
+}
+
+func EnoughPromResults(results []Result) error {
+	if len(results) < 1 {
+		return fmt.Errorf("prometheus query results must have at least 1 element")
+	}
+
+	return nil
+}
+
+func TotalPromCount(results []Result) (int, error) {
+	total := 0
+	for _, res := range results {
+		if len(res.Value) < 2 {
+			return 0, fmt.Errorf("result %v must have at least 2 elements", res)
+		}
+
+		val, err := strconv.Atoi(res.Value[1].(string))
+
+		if err != nil {
+			return 0, err
+		}
+
+		total += val
+	}
+
+	return total, nil
 }
