21cf21a08e36c04edb4788926a1c52db06983f52
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2023-09-11 11:08:29 +0200

    enable parallel test execution

--- yaml/yaml_suite_test.go
+++ yaml/yaml_suite_test.go
@@ -12,19 +12,17 @@ func TestYaml(t *testing.T) {
 	RunSpecs(t, "Yaml Suite")
 }
 
-var _ = Describe("test case", Ordered, ContinueOnFailure, Label("docker", "integration", "slow"), func() {
-	Describe("yaml test case", func() {
-		cases, base := yaml.ReadTestCases()
-		if base != "" {
-			It("should have at least one test case", func() {
-				Expect(cases).ToNot(BeEmpty(), "expected at least one test case in %s", base)
-			})
-		}
+var _ = Describe("test case", Label("docker", "integration", "slow"), func() {
+	cases, base := yaml.ReadTestCases()
+	if base != "" {
+		It("should have at least one test case", func() {
+			Expect(cases).ToNot(BeEmpty(), "expected at least one test case in %s", base)
+		})
+	}
 
-		for _, c := range cases {
-			Describe(c.Name, func() {
-				yaml.RunTestCase(c)
-			})
-		}
-	})
+	for _, c := range cases {
+		Describe(c.Name, Ordered, func() {
+			yaml.RunTestCase(c)
+		})
+	}
 })
