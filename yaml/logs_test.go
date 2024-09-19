61106205cbb99f5aabcf44a267cad09b241b6ce4
Gregor Zeitlinger <gregor.zeitlinger@grafana.com>
2024-09-19 14:44:32 +0200

    add kubernetes support

--- yaml/logs_test.go
+++ yaml/logs_test.go
@@ -11,20 +11,52 @@ func TestAssertLokiResponse(t *testing.T) {
 	file, err := os.ReadFile("testdata/loki_response.json")
 	require.NoError(t, err)
 	logs := ExpectedLogs{
-		Contains: []string{"Anonymous player is rolling the dice"},
+		Contains: []string{"simulating an error"},
 		Attributes: map[string]string{
-			"scope_name":        "com.grafana.example.RollController",
-			"service_name":      "dice",
-			"service_namespace": "shop",
-			"severity_number":   "9",
-			"severity_text":     "INFO",
+			"deployment_environment": "staging",
+			"exception_message":      "simulating an error",
+			"exception_type":         "java.lang.RuntimeException",
+			"scope_name":             "org.apache.catalina.core.ContainerBase.[Tomcat].[localhost].[/].[dispatcherServlet]",
+			"service_name":           "dice",
+			"service_namespace":      "shop",
+			"severity_number":        "17",
+			"severity_text":          "ERROR",
+			"k8s_container_name":     "dice",
+			"k8s_namespace_name":     "default",
+			"message":                "Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed: java.lang.RuntimeException: simulating an error] with root cause",
 		},
 		AttributeRegexp: map[string]string{
-			"span_id":  ".*",
-			"trace_id": ".*",
+			"thread_name":          ".*",
+			"span_id":              ".*",
+			"trace_id":             ".*",
+			"k8s_pod_name":         "dice-.*-.*",
+			"k8s_pod_uid":          ".*",
+			"exception_stacktrace": ".*",
 		},
 	}
 	AssertLokiResponse(gomega.NewGomega(func(message string, callerSkip ...int) {
 		t.Error(message)
 	}), file, logs, nil)
 }
+
+//contains:
+//  - 'simulating an error'
+//attributes:
+//  deployment_environment: staging
+//  exception_message: "simulating an error"
+//  exception_type: "java.lang.RuntimeException"
+//  scope_name: "org.apache.catalina.core.ContainerBase.[Tomcat].[localhost].[/].[dispatcherServlet]"
+//  service_name: dice
+//  service_namespace: shop
+//  severity_number: 17
+//  severity_text: ERROR
+//  k8s_container_name: dice
+//  k8s_namespace_name: shop
+//  message: "Servlet.service() for servlet [dispatcherServlet] in context with path [] threw exception [Request processing failed: java.lang.RuntimeException: simulating an error] with root cause"
+//attribute-regexp:
+//  thread_name: ".*"
+//  span_id: ".*"
+//  trace_id: ".*"
+//  k8s_pod_name: dice-.*-.*
+//  k8s_pod_uid: ".*"
+//  exception_stacktrace: ".*" # TODO: transform to sanitized stacktrace
